fa8a2b65c7a98061e5376e7464660547fc406065
firman dwi prabudi <firman.dp@mhs.unsyiah.ac.id>
2022-04-18 01:08:41 +0700

    Tampilan Log-In

--- web/login.php
+++ web/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 
     <title>Log-In | SIGAP</title>
 </head>
@@ -12,11 +12,10 @@
     <div class="box-login">
         <h2>LOG-IN</h2>
         <form action="" method="POST">
-            <input type="text" name="email" placeholdher="masukkan email"  class="input-control"><br>
-            <input type="password" name="password" placeholdher="password" class="input-control">
+            <input type="text" name="email" placeholdher="Masukkan Email"  class="input-control"><br>
+            <input type="password" name="password" placeholdher="Masukkan Password" class="input-control">
             <input type="submit" name="submit" value="Masuk" class="beton">
-            <link rel="stylesheet" href="./assets/style.css">
-            <p style="text-align : center;">Belum punya akun ? klik <a href="#">disini</a></p>
+            <p style="text-align : center;" class="daftar">Belum punya akun ? klik <a href="#">disini</a></p>
         </form>
         <?php
             if (isset($_POST['submit'])) {
