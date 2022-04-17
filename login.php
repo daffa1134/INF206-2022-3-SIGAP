c7a396c3328b5ed9d8aeeb90f7056d7c45921cac
firman dwi prabudi <firman.dp@mhs.unsyiah.ac.id>
2022-04-17 23:17:28 +0700

    Tampilan Log-in

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <title>Log-In | SIGAP</title>
+</head>
+<body id="bd-login">
+    <div class="box-login">
+        <h2>LOG-IN</h2>
+        <form action="" method="POST">
+            <input type="text" name="email" placeholdher="masukkan email"  class="input-control"><br>
+            <input type="password" name="password" placeholdher="password" class="input-control">
+            <input type="submit" name="submit" value="Masuk" class="beton">
+            <link rel="stylesheet" href="./assets/style.css">
+            <p style="text-align : center;">Belum punya akun ? klik <a href="#">disini</a></p>
+        </form>
+        <?php
+            if (isset($_POST['submit'])) {
+               $email = $_POST['email'];
+                $password = $_POST['password'];
+                $cek = mysqli_query($conn, "SELECT * FROM user WHERE email = '$email' AND password = '$password'");
+                
+                
+            }
+            ?>
+    </div>
+</body>
+</html>
