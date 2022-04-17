d10b3cb581b367f2f93e4be7b7bcd3453b2e826f
almaisashafira <shafira_a@mhs.unsyiah.ac.id>
2022-04-17 15:43:24 +0700

    commit sign up/daftar

---
+++ DaftarAdmin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset ="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"content="width=device-width,
+    initial-scale=1.0">
+    <title>SIGAP</title>
+    <link rel="stylesheet" href="daftar.css">
+</head>
+<body>
+<div class="container">
+<form action="" method="POST" class="daftar-form">
+    <p class ="daftar-text">DAFTAR</p>
+    <div class="input-group"><input type="text" placeholder="Nama Lengkap"
+    name="namalengkap"></div>
+    <div class="input-group"><input type="text" placeholder="Nama Apotek"
+    name="namaapotek"></div>
+    <div class="input-group"><input type="text" placeholder="E-mail"
+    name="email"></div>
+    <div class="input-group"><input type="password" placeholder="Password"
+    name="password"></div>
+    <div class="input-group"><input type="password" placeholder="Konfirmasi Password"
+    name="confirmpassword"></div>
+
+    <div class="input-group"><button name="submit" class="btn">DAFTAR</button></div>
+</form>
+</div>
+</body>
+</html>
