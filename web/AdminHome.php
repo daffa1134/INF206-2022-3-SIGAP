6d27956f692b3b32acf331034bf63170f96e6f84
daffa1134 <daffa1134@gmail.com>
2022-04-18 09:19:17 +0700

    Add AdminHome

---
+++ web/AdminHome.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Test Sidebar</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500&display=swap" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+        <link rel="stylesheet" href="../assets/css/jquery.mCustomScrollbar.min.css" />
+        <link rel="stylesheet" href="../assets/css/styleother.css" />
+        <link rel="stylesheet" href="../assets/css/style.css" />
+
+        <!-- Icon -->
+        <link rel="shortcut icon" href="./assets/ico/healthcare.png" type="image/x-icon" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+    </head>
+
+    <body>
+        <!-- Wrapper -->
+        <div class="wrapper">
+            <!-- Header -->
+            <header class="bg-second p-2">
+                <div class="container-fluid">
+                    <div class="d-flex flex-wrap align-items-center justify-content-between">
+                        <a class="btn btn-customized open-menu" href="#" role="button"> <i class="fas fa-bars"></i></a>
+
+                        <div class="dropdown text-end me-3">
+                            <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="../assets/img/pp-example.png" alt="photo profile" width="32" height="32" class="rounded-circle" />
+                            </a>
+                            <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+                                <li><a class="dropdown-item" href="#">Settings</a></li>
+                                <li><a class="dropdown-item" href="#">Profile</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+            <!-- Sidebar -->
+            <nav class="sidebar">
+                <!-- close sidebar menu -->
+                <div class="dismiss">
+                    <i class="fas fa-times"></i>
+                </div>
+
+                <ul class="list-unstyled menu-elements mt-5">
+                    <li class="active">
+                        <a class="scroll-link mb-2" href="#"><i class="fas fa-home"></i> Home</a>
+                    </li>
+                    <li>
+                        <a class="scroll-link mb-2" href="#"><i class="fas fa-info-circle"></i> Tentang</a>
+                    </li>
+                    <li>
+                        <a class="scroll-link mb-2" href="#"><i class="fas fa-question-circle"></i> Bantuan</a>
+                    </li>
+                    <li>
+                        <a class="scroll-link mb-2" href="#"><i class="fas fa-power-off"></i> Keluar</a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- End sidebar -->
+
+            <!-- ISi konten -->
+            <div class="container mt-3">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod odit explicabo iure nisi temporibus eveniet minus iste voluptates quisquam vel debitis earum, possimus assumenda. Dignissimos exercitationem sunt eos
+                    asperiores iure ipsa velit sed ut voluptates sequi voluptas, voluptatibus architecto hic repellendus fugit eum quos. Sed ad, ducimus consequuntur quaerat, molestiae laudantium autem labore illum accusamus itaque nobis
+                    voluptates saepe facilis, odio voluptatum eligendi debitis ex officiis ipsum voluptas. Veritatis a animi neque maxime reprehenderit voluptas, id architecto expedita quaerat aspernatur consequatur in, voluptatem enim
+                    quibusdam suscipit sunt ullam pariatur dicta laborum, tempore nihil temporibus voluptatum! Distinctio, optio. Deserunt perferendis nulla soluta eius est ipsa reprehenderit distinctio autem blanditiis vero error,
+                    recusandae quaerat totam repellat numquam vel quod rem eaque non temporibus at placeat illo? Dolor ipsum perferendis error nulla ea provident dolorum tempore porro sint, esse, qui exercitationem, magnam vel accusamus
+                    aperiam deserunt. Sapiente sed mollitia architecto maiores velit numquam dolorum recusandae totam quis deleniti hic facere excepturi nesciunt magnam quibusdam illo, nostrum aperiam odit aspernatur minus. Tempora nobis
+                    dolore distinctio hic, facere error dicta quis libero provident, ullam odit consequatur sit recusandae accusantium dolor sequi adipisci officia eligendi ea voluptas placeat cumque. Consequuntur, recusandae libero sint
+                    excepturi deleniti saepe!
+                </p>
+            </div>
+        </div>
+        <!-- End Wrapper -->
+        <!-- Javascript -->
+        <script src="../assets/js/jquery-3.3.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="../assets/js/scripts.js"></script>
+    </body>
+</html>
