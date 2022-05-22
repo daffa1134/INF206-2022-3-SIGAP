0ee9f7e337ae585584fe64ef1df1a6a6e1f33547
miidah <Nurhamidah@mhs.unsyiah.ac.id>
2022-05-23 04:26:51 +0700

    memperbarui tampilan profil dokter

---
+++ web/Modifikasi_datadokter.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Modifikasi Informasi Dokter</title>
+
+	<!-- CSS -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500&display=swap" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="../assets/css/jquery.mCustomScrollbar.min.css" />
+    <link rel="stylesheet" href="../assets/css/styleother.css" />
+    <link rel="stylesheet" href="../assets/css/style.css" />
+
+    <!-- Icon -->
+    <link rel="shortcut icon" href="../assets/ico/healthcare.png" type="image/x-icon" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+
+</head>
+<body>
+ <!-- Wrapper -->
+    <div class="wrapper">
+        <!-- Header -->
+        <header class="bg-second p-2">
+            <div class="container-fluid">
+                <div class="d-flex flex-wrap align-items-center justify-content-between">
+                    <a class="btn btn-customized open-menu" href="#" role="button"> <i class="fas fa-bars"></i></a>
+                    <div class="dropdown btn-group me-3">
+                        <span class="me-2" style="font-weight: 600;">Firman</span>
+                        <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="../assets/img/pp-example.png" alt="photo profile" width="32" height="32" class="rounded-circle" />
+                        </a>
+                        <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+                            <li><a class="dropdown-item" href="#">Settings</a></li>
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- End Wrapper -->
+
+		<!-- Sidebar -->
+        <nav class="sidebar">
+            <!-- close sidebar menu -->
+            <div class="dismiss">
+                <i class="fas fa-times"></i>
+            </div>
+
+            <ul class="list-unstyled menu-elements mt-5">
+                <li class="active">
+                    <a class="scroll-link mb-2" href="#"><i class="fas fa-home"></i> Home</a>
+                </li>
+                <li>
+                    <a class="scroll-link mb-2" href="#"><i class="fas fa-info-circle"></i> Tentang</a>
+                </li>
+                <li>
+                    <a class="scroll-link mb-2" href="#"><i class="fas fa-question-circle"></i> Bantuan</a>
+                </li>
+                <li>
+                    <a class="scroll-link mb-2" href="#"><i class="fas fa-power-off"></i> Keluar</a>
+                </li>
+            </ul>
+        </nav>
+        <!-- End sidebar -->
+
+        <!--Isi Konten-->
+        <div class="container mt-3">
+        <div class="panel-body inf-content">
+        <div class="row">
+        <div class="col-md-4">
+            <img alt="" style="width:600px;" title="" class="img-circle img-thumbnail isTooltip" src="https://bootdey.com/img/Content/avatar/avatar7.png" data-original-title="Usuario"> 
+        
+            <ul title="Ratings" class="list-inline ratings text-center">
+                <li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-star"></span></a></li>
+            </ul>
+        </div>
+        <div class="col-md-6">
+            <h3>Rumah Sakit Dr. Aminah Seodarso</h3>
+            <strong>Informasi Dokter</strong><br>
+            <div class="table-responsive">
+            <table class="table table-user-information">
+                <tbody>
+                    <tr>        
+                        <td>
+                            <strong>
+                                <span class="glyphicon glyphicon-asterisk text-primary"></span>
+                                NIP                                               
+                            </strong>
+                        </td>
+                        <td class="form-group">
+                            <input type="text" class="form-control" id="NIP" placeholder="Masukkan NIP">     
+                        </td>
+                    </tr>
+                    <tr>    
+                        <td>
+                            <strong>
+                                <span class="glyphicon glyphicon-user  text-primary"></span>    
+                                Nama                                                
+                            </strong>
+                        </td>
+                        <td class="form-group">
+                            <input type="text" class="form-control" id="Nama" placeholder="Masukkan Nama"> 
+                        </td>
+                    </tr>
+
+                    <tr>        
+                        <td>
+                            <strong>
+                                <span class="glyphicon glyphicon-eye-open text-primary"></span> 
+                                Email                                              
+                            </strong>
+                        </td>
+                        <td class="form-group">
+                            <input type="text" class="form-control" id="Email" placeholder="Masukkan Email"> 
+                        </td>
+                    </tr>
+                    <tr>        
+                        <td>
+                            <strong>
+                                <span class="glyphicon glyphicon-envelope text-primary"></span> 
+                                Spesialis                                                
+                            </strong>
+                        </td>
+                        <td class="form-group">
+                            <input type="text" class="form-control" id="Spesialis" placeholder="Masukkan Speasialis"> 
+                        </td>
+                    </tr>
+                    <tr>        
+                        <td>
+                            <strong>
+                                <span class="glyphicon glyphicon-calendar text-primary"></span>
+                                Jam Kerja                                               
+                            </strong>
+                        </td>
+                        <td class="form-group">
+                            <input type="text" class="form-control" id="Jam Kerja" placeholder="Masukkan Jam Kerja"> 
+                        </td>
+                    </tr>
+                    <tr>        
+                        <td>
+                            <strong>
+                                <span class="glyphicon glyphicon-calendar text-primary"></span>
+                                Alamat                                               
+                            </strong>
+                        </td>
+                        <td class="form-group">
+                            <input type="text" class="form-control" id="Alamat" placeholder="Masukkan Alamat"> 
+                        </td>
+                    </tr>                                    
+                </tbody>
+            </table>
+            <button type="button" id="submit" name="submit" class="btn btn-primary">Update</button>
+            </div>
+        </div>
+    </div>
+</div>
+</div>                                        
+
+    <!-- Javascript -->
+    <script src="../assets/js/jquery-3.3.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="../assets/js/scripts.js"></script>
+</body>
+</html>
